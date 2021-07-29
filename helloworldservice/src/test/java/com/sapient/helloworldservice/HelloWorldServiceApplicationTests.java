3cab9c06130814c927f337540377ef9e145a30ea
Mayank Arora <mayankarora66@yahoo.com>
2021-07-29 10:43:44 +0530

    added Dockerfile and sample test cases

--- helloworldservice/src/test/java/com/sapient/helloworldservice/HelloWorldServiceApplicationTests.java
+++ helloworldservice/src/test/java/com/sapient/helloworldservice/HelloWorldServiceApplicationTests.java
@@ -1,5 +1,7 @@
 package com.sapient.helloworldservice;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,18 @@ class HelloWorldServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	void test1() {
+//		sample test
+		assertTrue(1 == 1);
+	}
+	
+	@Test
+	void test2() {
+//		sample test
+		assertTrue(2 == 2);
 	}
 
 }
